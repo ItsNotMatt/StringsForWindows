1914f494c073af48bba9cb2e5538573a179d4741
Matt <mmancarella23@gmail.com>
2024-02-24 17:15:30 -0600

    added basic CLI

--- main.go
+++ main.go
@@ -14,17 +14,40 @@ func main() {
         log.Fatal("Error getting current dir")
     }
 
-    filepath := filepath.Join(dir, "test_exe", "strings_windows.exe")
-
-    content, err := os.ReadFile(filepath)
+    path, cont := parseFirstArg(os.Args[1])
+    if !cont {
+        return
+    }
+    path = filepath.Join(dir, path)
+    
+    var dumpFile *os.File
+    var dump bool
+    if len(os.Args) > 2 {
+        dumpFile, dump = parseSecondArg(dir, os.Args[2]) 
+    }
+    
+    content, err := os.ReadFile(path)
     if err != nil {
         log.Fatal("Error reading file", err)
     }
 
-    parseFile(string(content), true)
+    strings := parseFile(string(content), true)
+
+    if dump {
+        for _, str := range strings {
+            _, err := dumpFile.WriteString(str + "\n")
+            if err != nil {
+                log.Fatal("Err writing to file", err)
+            }
+        } 
+    } else {
+        for _, str := range strings {
+            fmt.Println(str)
+        }
+    }
 }
 
-func parseFile(content string, writeToFile bool) {
+func parseFile(content string, writeToFile bool) []string {
     chars := []rune(content)
     var tempChars []rune
     var strings []string
@@ -47,27 +70,38 @@ func parseFile(content string, writeToFile bool) {
             addToStrings = false
         }
     }
+    return strings
+}
 
-    if writeToFile {
-        dir, _ := filepath.Abs(filepath.Dir("."))
-        filepath := filepath.Join(dir, "dump.txt")
 
+func parseFirstArg(arg string) (string, bool) {
+    if arg == "help" {
+        fmt.Println("Commands: ")
+        return "", false
+    } else {
+        //assuming its a filename now
+        return arg, true
+    }
+}
+
+func parseSecondArg(dir string, arg string) (*os.File, bool) {
+    if arg == ">" {
+        filepath := filepath.Join(dir, os.Args[3])
         file, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE, 0644)
         if err != nil {
             log.Fatal("Error opening file", err)
         }
         defer file.Close()
 
-        for _, str := range strings {
-            _, err := file.WriteString(str + "\n")
-            if err != nil {
-                log.Fatal("Err writing to file", err)
-            }
-        } 
+        //assuming its a filename now
+        return file, true
+    } else if arg == "-idkwhatyet" {
+        //dont continue after tick
+        return nil, false
     } else {
-        for _, str := range strings {
-            fmt.Println(str)
-        }
+        return nil, false
     }
-
 }
+
+
+
